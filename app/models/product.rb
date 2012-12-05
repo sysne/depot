614ba20a2ff3f9027c97caaad657029ea3e5f064
k.tashiro <k.tashiro@aktsk.jp>
2012-12-05 10:48:58 +0900

    cart coding.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,23 @@
+# encoding: utf-8
 class Product < ActiveRecord::Base
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, :description, :image_url, presence: true
-  attr_accessible :description, :image_url, :price, :title
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, '品目が存在します')
+        return false
+      end
+    end
+
+#  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+#  validates :title, :description, :image_url, presence: true
+# validates :title, uniqueness: true
+#  validates :image_url, allow_blank: true, format: { with: %r{\ .(gif|jpg|png)$}i, message: 'はGIF、JPG、PNG画 像 のURLで な け れ ば な り ま せ ん' }
+#  attr_accessible :description, :image_url, :price, :title
 end
